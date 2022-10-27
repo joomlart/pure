310b06d02c851fea37efe742d98bef82165725c4
Nguyễn Duy Công <cnd.congnguyenduy@gmail.com>
2022-10-27 11:10:46 +0700

    update

--- assets/blog-categories-custom.js
+++
@@ -1,9 +0,0 @@
-let getCategory =  document.querySelectorAll(".category-item > a");
-let arr_Cat = [...getCategory];
-arr_Cat.map((item) => {
-  let cutstring = item.innerHTML;
-  cutstring = cutstring.replace('/blogs/', '');
-  cutstring = cutstring.replace('-', ' ')
-  item.innerHTML = cutstring;
-}
-);
